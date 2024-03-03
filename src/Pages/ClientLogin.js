70c7983e1fdeb1c862ac203fad21b6f368cfb4c3
Gopinath Vasan <vasangopinath11@gmail.com>
2024-03-03 19:40:31 +0530

    commit

--- src/Pages/ClientLogin.js
+++ src/Pages/ClientLogin.js
@@ -1,72 +1,83 @@
-import React, { useState } from 'react';
-import './clientLoginStyle.css';
-import './LanguageSelection';
+import React, { useState } from "react";
+import axios from "axios";
+// import SuccessLogin from "./SuccessLogin";
+import Homepage from "../ComponentsPages/HomePage";
 import Navbar from "../Components/Navbar";
 import Footer from '../Components/Footer';
+import './clientLoginStyle.css';
+
 
 const ClientLogin = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-
-    // Here you can add the logic to send the sign data to your backend or handle it as needed
-    console.log('Signing data:', { username, password });
-
-    // Optionally, you can reset the form fields
-    setUsername('');
-    setPassword('');
+    try {
+      const response = await axios.post('http://localhost:8000/login', {
+        username: username,
+        password: password
+      });
+      if (response && response.data) {
+        setMessage(response.data.message);
+        setIsLoggedIn(true); // Set isLoggedIn state to true
+      } else {
+        setMessage('Error: invalid response from server');
+      }
+    } catch (error) {
+      setMessage(error.message);
+    }
   };
 
   return (
     <>
-    <Navbar />
-    <div className="client-login-container">
-      {/* <h1>Austin Partnership</h1> */}
-      <h2>Welcome to Austin Partnership</h2>
-      <form className='login-form' onSubmit={handleSubmit}>
-        <div className="form-group">
-          <label className='login-label' htmlFor="username">Email-ID:</label>
-          <input
-            className='login-input'
-            type="text"
-            id="username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            placeholder="Enter username or email address"
-            required
-          />
-        </div>
+    {isLoggedIn ? (
+      <Homepage />
+    ) : (
+    <>
+      <Navbar />
+      <div className="client-login-container">
+        {/* <h2>Welcome to Austin Partnership</h2> */}
+       
+          <form className='login-form' onSubmit={handleSubmit}>
+            <div className="form-group">
+              <label className='login-label' htmlFor="username">Email-ID:</label>
+              <input
+                className='login-input'
+                type="text"
+                id="username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                placeholder="Enter username or email address"
+                required
+              />
+            </div>
 
-        <div className="form-group">
-          <label className='login-label' htmlFor="password">Password:</label>
-          <input
-          className='login-input'
-            type="password"
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            placeholder="Enter password"
-            required
-          />
-        </div>
+            <div className="form-group">
+              <label className='login-label' htmlFor="password">Password:</label>
+              <input
+                className='login-input'
+                type="password"
+                id="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="Enter password"
+                required
+              />
+            </div>
 
-        <button className='login-btn' type="submit">Sign In</button>
-      {/* <div className="Remember me">
-        <a className='login-a' href="#">Remember Me</a>
-      </div> */}
-      
-      <div className="forgot-password">
-        <a className='login-a' href="#">Forgot password?</a>
+            <button className='login-btn' type="submit">Sign In</button>
+            {message && <p>{message}</p>}
+          </form>
+       
       </div>
-       <div className="login-Support">
-        <a className='login-a' href="#">Contact Technical Support</a>
-      </div>
-      </form>
-    </div>
-    <Footer />
+      <Footer />
     </>
+    )
+  }
+  </>
   );
 };
 
---
+++ src/Pages/SuccessLogin.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './SuccessLogin.css';
+
+const SuccessLogin = () => {
+  return (
+    <div className="success-login-container">
+      <h2>Login Successful!</h2>
+      <p>Welcome to the Austin Partnership.</p>
+    </div>
+  );
+};
+
+export default SuccessLogin;
